15e524dddc287f7ce9231dcc6ac7e75de551a16a
KoenKwakkenbos <k.kwakkenbos@gmail.com>
2023-01-19 13:00:11 +0100

    Add slurm to test on cluster.

---
+++ slurm_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --ntasks=1           ### How many CPU cores do you need?
+#SBATCH --mem=14G            ### How much RAM memory do you need?
+#SBATCH -p express           ### The queue to submit to: express, short, long, interactive
+#SBATCH --gres=gpu:1         ### How many GPUs do you need?
+#SBATCH -t 0-01:00:00        ### The time limit in D-hh:mm:ss format
+#SBATCH -o /trinity/home/kkwakkenbos/repositories/EMC-Seminoma-Relapse/out_%j.log        ### Where to store the console output (%j is the job number)
+#SBATCH -e /trinity/home/kkwakkenbos/repositories/EMC-Seminoma-Relapse/error_%j.log      ### Where to store the error output
+#SBATCH --job-name=datagen_test  ### Name your job so you can distinguish between jobs
+
+# Load the modules
+
+module purge
+# automatically loads all dependencies such as cuda
+# replace with required tensorflow version! (check with module avail which tensorflow version are available)
+module load TensorFlow/2.2.0-fosscuda-2019b-Python-3.7.4  
+
+# use when you need to read/write many files quickly in tmp directory:
+# source /tmp/${SLURM_JOB_USER}.${SLURM_JOB_ID}/prolog.env
+
+# activate virtualenv after loading tensorflow/python module
+# replace with your own virtualenv!
+source /trinity/home/kkwakkenbos/venv_example_project/bin/activate 
+echo "Hello world"
+
+python testgenerator.py
