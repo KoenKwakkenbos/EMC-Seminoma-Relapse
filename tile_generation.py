4e55f051e9976e976cd0114e56c037d0702c7f83
KoenKwakkenbos <k.kwakkenbos@gmail.com>
2023-01-29 13:36:02 +0100

    Can now handle QuPath geometries as well.

--- tile_generation.py
+++ tile_generation.py
@@ -58,7 +58,7 @@ def tile_slide(annotation_path, tile_size, downsample):
     with open(annotation_path, 'r', encoding="utf8") as file:
         annotation = json.load(file)
 
-    annotation_polys = [shape(f["geometry"]) for f in annotation["features"]]
+    annotation_polys = [poly for poly in list(shape(feat["geometry"]).geoms) for feat in annotation["features"]]
 
     # initialize bounding box around first annotation
     bbox = annotation_polys[0].bounds
