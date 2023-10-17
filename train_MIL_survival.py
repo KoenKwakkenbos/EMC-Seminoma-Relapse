66f147782bc9a308c8149814bfa3a545d576ae4d
Koen Kwakkenbos <k.kwakkenbos@gmail.com>
2023-10-17 17:25:15 +0200

    MIL survival

--- train_MIL_survival.py
+++ train_MIL_survival.py
@@ -25,15 +25,38 @@ import tensorflow as tf
 from tensorflow import keras
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
+from sklearn.utils import resample
 import tensorflow.keras.backend as K
+import wandb
 
 from datagenerator_survival import DatagenMILSurv
-from models import create_MIL_model
+from models import create_MIL_model, create_classification_model
 from helpers import dir_path, tar_path
 from survival_model import CoxPHLoss, CindexMetric
 from pathlib import Path
 
 import config
+
+print('starting')
+# Start a run, tracking hyperparameters
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="survival-mil-resnet",
+
+    # track hyperparameters and run metadata with wandb.config
+    config={
+        "layer_1": 512,
+        "activation_1": "relu",
+        # "dropout": random.uniform(0.01, 0.80),
+        "layer_2": 10,
+        "activation_2": "softmax",
+        "optimizer": "sgd",
+        "loss": "sparse_categorical_crossentropy",
+        "metric": "accuracy",
+        "epoch": 8,
+        "batch_size": 256
+    }
+)
 
 def parse_arguments():
     """
@@ -71,6 +94,7 @@ class TrainAndEvaluateModel:
         self.loss_fn = CoxPHLoss()
 
         self.train_loss_metric = tf.keras.metrics.Mean(name="train_loss")
+        self.train_cindex_metric = CindexMetric()
         self.val_loss_metric = tf.keras.metrics.Mean(name="val_loss")
         self.val_cindex_metric = CindexMetric()
 
@@ -101,7 +125,8 @@ class TrainAndEvaluateModel:
         # Only pass the samples through and collect the output vales
         return model(x, training=False)
 
-    def train_one_epoch(self, step_counter):
+    def train_one_epoch(self, step_counter, epoch):
+        self.train_cindex_metric.reset_states()
         logits = tf.zeros([0, 1])
         for x, y in tqdm(self.train_ds):
             logits_batch = self.inference_step(x)
@@ -119,28 +144,31 @@ class TrainAndEvaluateModel:
             train_loss, logits = self.train_one_step(
                 x, y["label_event"], y["label_riskset"])
 
-        step = int(step_counter)
-        if step == 0:
-            func = self.train_one_step.get_concrete_function(
-                x, y["label_event"], y["label_riskset"]
-            )
+            step = int(step_counter)
+            # if step == 0:
+            #     func = self.train_one_step.get_concrete_function(
+            #         x, y["label_event"], y["label_riskset"]
+            #     )
 
-        self.train_loss_metric.update_state(train_loss)
-        if step % 1 == 0:
-            # Display metrics
-            mean_loss = self.train_loss_metric.result()
-            print(f"step {step}: mean loss = {mean_loss:.4f}")
-            # Reset training metrics
-            self.train_loss_metric.reset_states()
+            self.train_loss_metric.update_state(train_loss)
+            self.train_cindex_metric.update_state(y, logits)
+            step_counter.assign_add(1)
 
-        step_counter.assign_add(1)
+
+        # Display metrics
+        mean_loss = self.train_loss_metric.result()
+        train_cindex = self.train_cindex_metric.result()
+        self.log_metrics([train_cindex['cindex']], 'train', 'pat', mean_loss)
+        print(f"Epoch {step}: mean loss = {mean_loss:.4f}, train cindex = {train_cindex['cindex']:.4f}")
+        # Reset training metrics
+        self.train_loss_metric.reset_states()
+
+        
   
     @tf.function
     def evaluate_one_step(self, x, y_event, y_riskset):
         y_event = tf.expand_dims(y_event, axis=1)
         val_logits = model(x, training=False)
-        # val_acc_metric.update_state(y, val_logits)
-        # val_auc_metric.update_state(y, tf.nn.sigmoid(val_logits))
 
         val_loss = self.loss_fn(y_true=[y_event, y_riskset], y_pred=val_logits)
         return val_loss, val_logits
@@ -164,7 +192,7 @@ class TrainAndEvaluateModel:
             val_loss, val_logits = self.evaluate_one_step(
                 x_val, y_val["label_event"], y_val["label_riskset"]
             )
-
+            
             # Update val metrics
             self.val_loss_metric.update_state(val_loss)
             self.val_cindex_metric.update_state(y_val, val_logits)
@@ -178,6 +206,7 @@ class TrainAndEvaluateModel:
         self.val_loss_metric.reset_states()
 
         val_cindex = self.val_cindex_metric.result()
+        self.log_metrics([val_cindex['cindex']], 'val', 'pat', val_loss)
 
         print(f"Validation: loss = {val_loss:.4f}, cindex = {val_cindex['cindex']:.4f}")
 
@@ -188,7 +217,7 @@ class TrainAndEvaluateModel:
             model=self.model)
         
         for epoch in range(self.num_epochs):
-            self.train_one_epoch(ckpt.step)
+            self.train_one_epoch(ckpt.step, epoch)
             self.evaluate(ckpt.step)
             self.train_ds.on_epoch_end()
             self.val_ds.on_epoch_end()
@@ -235,6 +264,20 @@ class TrainAndEvaluateModel:
         #  Adapted to return index!
         # return list(order[index])
         return np.where(index)[0]
+    
+    def log_metrics(self, metrics, split, prefix, loss):
+        wandb.log({
+            f"{prefix}_{split}_loss": loss,
+            f"{prefix}_{split}_cindex": metrics[0],
+            # f"{prefix}_{split}_f1": metrics[2],
+            # f"{prefix}_{split}_balacc": metrics[1],
+            # f"{prefix}_{split}_recall": metrics[4],
+            # f"{prefix}_{split}_precision": metrics[3],
+            # f"{prefix}_{split}_cnfmatrix": metrics[6],
+            # f"{prefix}_{split}_auc": metrics[5]
+            #f"{prefix}_{split}_fpr": metrics[7],
+            #f"{prefix}_{split}_tpr": metrics[8]
+        })
 
 
 if __name__ == "__main__":
@@ -252,28 +295,22 @@ if __name__ == "__main__":
     print(f"  Train: index={train_index}")
     print(f"  Test:  index={test_index}")
 
-    print(patient_data.loc[train_index])
+    INPUT_PATH = './tiles-normalized/Training/Oversampled'
 
-    INPUT_PATH = './tiles_TCGA_normalized'
-
-    train_gen = DatagenMILSurv(list(patient_data.loc[train_index].index),
-                           patient_data['Event'].loc[train_index],
-                           patient_data['Time'].loc[train_index],
+    train_gen = DatagenMILSurv(patient_data,
                            512,
                            INPUT_PATH,
                            batch_size=32,
                            train=True,
                            imagenet=False)
-    val_gen = DatagenMILSurv(list(patient_data.loc[test_index].index),
-                         patient_data['Event'].loc[test_index],
-                         patient_data['Time'].loc[test_index],
+    val_gen = DatagenMILSurv(patient_data,
                          512,
                          INPUT_PATH,
                          batch_size=32,
                          train=False,
                          imagenet=False)
 
-    model = create_MIL_model(input_shape=(512, 512, 3))
+    model = create_classification_model(input_shape=(512, 512, 3))
 
     trainer = TrainAndEvaluateModel(
         model=model,
